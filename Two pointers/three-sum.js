072339c08ffa33335dc2bc54e94845a44c7a4a73
adimishra6003 <adityamishra120799@gmail.com>
2025-10-25 01:36:24 +0530

    2 pointer approach

--- Two pointers/three-sum.js
+++ Two pointers/three-sum.js
@@ -20,3 +20,38 @@ var threeSum = function (nums) {
 };
 
 // 2 pointer approach O(n^2)
+
+var threeSum = function (nums) {
+  nums.sort((a, b) => a - b);
+  let res = [],
+    sum = 0;
+  let n = nums.length;
+
+  let left, right;
+  for (let i = 0; i < n - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    left = i + 1;
+    right = n - 1;
+
+    while (left < right) {
+      sum = nums[i] + nums[left] + nums[right];
+
+      if (sum == 0) {
+        res.push([nums[i], nums[left], nums[right]]);
+
+        // Skip duplicate left & right values
+        while (left < right && nums[left] === nums[left + 1]) left++;
+        while (left < right && nums[right] === nums[right - 1]) right--;
+        left++;
+        right--;
+      } else if (sum < 0) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return res;
+};
