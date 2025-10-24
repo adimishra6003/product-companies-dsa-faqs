1be6e7153933ef29786dc49c47defee3ae254d00
adimishra6003 <adityamishra120799@gmail.com>
2025-10-25 00:25:37 +0530

    2 pointer approach

--- Two pointers/remove-element.js
+++ Two pointers/remove-element.js
@@ -1,4 +1,5 @@
 // Naive approach using extra array
+// TC - O(n)
 
 var removeElement = function (nums, val) {
   let newArr = [];
@@ -14,4 +15,20 @@ var removeElement = function (nums, val) {
   }
 
   return newArr.length;
+};
+
+// 2 pointer approach without using extra space
+//TC - O(n)
+
+var removeElement = function (nums, val) {
+  let k = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+
+  return k;
 };
