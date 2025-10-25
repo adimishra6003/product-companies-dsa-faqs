fea595072ab73db082c75cad7600186864451ee7
adimishra6003 <adityamishra120799@gmail.com>
2025-10-26 01:27:48 +0530

    2 pointer apporach

--- Two pointers/minimum-size-subarray-sum.js
+++ Two pointers/minimum-size-subarray-sum.js
@@ -18,3 +18,24 @@ var minSubArrayLen = function (target, nums) {
 
   return minLen === Infinity ? 0 : minLen;
 };
+
+// Sliding window (Two pointer approach) keep sliding right pointer till sum>=target, and when condition becomes true, move left pointer forward and reduce them from sum
+// TC => O(n)
+
+var minSubArrayLen = function (target, nums) {
+  let left = 0,
+    sum = 0,
+    minLen = Infinity;
+
+  for (let right = 0; right < nums.length; right++) {
+    sum += nums[right];
+
+    while (sum >= target) {
+      minLen = Math.min(minLen, right - left + 1);
+      sum -= nums[left];
+      left++;
+    }
+  }
+
+  return minLen === Infinity ? 0 : minLen;
+};
