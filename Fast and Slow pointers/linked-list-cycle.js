bb7dcba8b3ad39e5b70c200d831dda480683d779
adimishra6003 <adityamishra120799@gmail.com>
2025-10-26 19:43:36 +0530

    fast slow approach

--- Fast and Slow pointers/linked-list-cycle.js
+++ Fast and Slow pointers/linked-list-cycle.js
@@ -1,5 +1,5 @@
 // Naive approach using extra space (set)
-// TC => O(n)
+// TC => O(n), SC => O(n)
 
 var hasCycle = function (head) {
   let seen = new Set();
@@ -8,6 +8,24 @@ var hasCycle = function (head) {
     if (seen.has(head)) return true;
     seen.add(head);
     head = head.next;
+  }
+
+  return false;
+};
+
+// Fast and slow pointer approach (Floyd's cycle detection algo)
+// TC => O(n), SC => O(1)
+
+var hasCycle = function (head) {
+  if (!head) return false;
+
+  let slow = head;
+  let fast = head;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
   }
 
   return false;
