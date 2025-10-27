317c7b0728780bba80c66a63c7a0a7f168099da8
adimishra6003 <adityamishra120799@gmail.com>
2025-10-27 23:41:41 +0530

    fast and slow pointer approach

--- Fast and Slow pointers/first-node-in-cycle-in-LL.js
+++ Fast and Slow pointers/first-node-in-cycle-in-LL.js
@@ -17,4 +17,35 @@ var detectCycle = function (head) {
   return null;
 };
 
-//
+// Fast slow pointer approach, when you get the point of collision of fast and slow pointer, start a new pointer
+//  "entry" from head and keep slow and entry moving one step till they meet each other at the first node of
+// the loop (It will always collide at the starting of loop, we can prove mathematically)
+
+var detectCycle = function (head) {
+  if (head == null || head.next == null) return null;
+
+  let slow = head;
+  let fast = head;
+  let hasCycle = false;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow == fast) {
+      hasCycle = true;
+      break;
+    }
+  }
+
+  if (!hasCycle) return null;
+
+  let entry = head;
+
+  while (entry !== slow) {
+    slow = slow.next;
+    entry = entry.next;
+  }
+
+  return entry;
+};
