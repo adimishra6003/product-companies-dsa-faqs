214fa307073a3b7d0f620f8344e97e685ff0347e
adimishra6003 <adityamishra120799@gmail.com>
2025-10-29 23:58:09 +0530

    fast and slow pointer approach added

--- Fast and Slow pointers/Palindrome-linked-list.js
+++ Fast and Slow pointers/Palindrome-linked-list.js
@@ -21,3 +21,38 @@ var isPalindrome = function (head) {
 
   return true;
 };
+
+// Fast and Slow pointer approach without using set. First find the middle, then reverse from the middle, and then compare the first and second (reversed) half
+// TC => O(n), SC => O(1)
+
+var isPalindrome = function (head) {
+  if (!head || !head.next) return true;
+
+  let fast = head,
+    slow = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  let prev = null;
+  while (slow) {
+    let next = slow.next;
+    slow.next = prev;
+    prev = slow;
+    slow = next;
+  }
+
+  let left = head,
+    right = prev;
+
+  while (right) {
+    if (left.val !== right.val) return false;
+
+    left = left.next;
+    right = right.next;
+  }
+
+  return true;
+};
