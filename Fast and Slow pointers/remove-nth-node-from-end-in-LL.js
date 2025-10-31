b507984105137dae572079fae7e3607315b33f76
adimishra6003 <adityamishra120799@gmail.com>
2025-10-31 23:33:03 +0530

    fast and slow pointer approach

--- Fast and Slow pointers/remove-nth-node-from-end-in-LL.js
+++ Fast and Slow pointers/remove-nth-node-from-end-in-LL.js
@@ -1,4 +1,4 @@
-// Naive approach using counting
+// Naive approach using counting (TC is same as Fast and slow pointer, but it needs 2 passes)
 // TC => O(n), SC => O(n)
 
 var removeNthFromEnd = function (head, n) {
@@ -21,4 +21,26 @@ var removeNthFromEnd = function (head, n) {
   curr.next = curr.next.next; // When curr is pointing at the node before the node to be deleted, make it's next to be next to next
 
   return head;
+};
+
+// Fast and Slow pointer approach, we use dummy node here to counter the cases where the head node may be removed
+// TC => O(n) Single pass, SC => O(1)
+
+var removeNthFromEnd = function (head, n) {
+  let dummy = new ListNode(0);
+  dummy.next = head;
+  let fast = dummy;
+  let slow = dummy;
+
+  for (let i = 0; i <= n; i++) {
+    fast = fast.next;
+  }
+
+  while (fast) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+  slow.next = slow.next.next;
+
+  return dummy.next;
 };
