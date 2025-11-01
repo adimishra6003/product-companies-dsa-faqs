6c017220a574f471be2774091427575134cb5431
adimishra6003 <adityamishra120799@gmail.com>
2025-11-01 23:45:52 +0530

    optimized approach

--- Fast and Slow pointers/reorder-list.js
+++ Fast and Slow pointers/reorder-list.js
@@ -23,3 +23,43 @@ var reorderList = function (head) {
   }
   arr[i].next = null;
 };
+
+// Fast and Slow pointer approach -> Break the LL into 2 halves, reverse the second half and then join their nodes alternatively
+// TC => O(n), SC => O(1)
+
+var reorderList = function (head) {
+  if (!head || !head.next) return head;
+
+  let slow = head,
+    fast = head;
+  // Finding middle node
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  // Reversing the second half
+  let prev = null,
+    curr = slow.next;
+  slow.next = null; // Breaking the first half's connection
+  while (curr) {
+    let nextNode = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = nextNode;
+  }
+
+  // Rearranging the LL by taking one node from each LL alternatively
+
+  let first = head,
+    second = prev;
+  while (second) {
+    let tmp1 = first.next;
+    let tmp2 = second.next;
+    first.next = second;
+    second.next = tmp1;
+
+    first = tmp1;
+    second = tmp2;
+  }
+};
