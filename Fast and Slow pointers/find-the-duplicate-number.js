7b4ef1289c030aa6e1878c79352b785334bf101a
adimishra6003 <adityamishra120799@gmail.com>
2025-11-02 23:52:45 +0530

    optimized approach

--- Fast and Slow pointers/find-the-duplicate-number.js
+++ Fast and Slow pointers/find-the-duplicate-number.js
@@ -20,3 +20,25 @@ var findDuplicate = function (nums) {
     seen.add(nums[i]);
   }
 };
+
+//
+
+var findDuplicate = function (nums) {
+  let slow = nums[0];
+  let fast = nums[0];
+
+  // Phase 1: Find intersection point
+  do {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+  } while (slow !== fast);
+
+  // Phase 2: Find entrance to the cycle (duplicate)
+  slow = nums[0];
+  while (slow !== fast) {
+    slow = nums[slow];
+    fast = nums[fast];
+  }
+
+  return slow; // or fast
+};
