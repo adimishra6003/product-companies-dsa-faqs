13746254343323e9f756d0b14e01c47d670bdae2
adimishra6003 <adityamishra120799@gmail.com>
2025-11-05 21:41:07 +0530

    sliding window approach

--- Sliding window/maximum-average-subarray-sum-1.js
+++ Sliding window/maximum-average-subarray-sum-1.js
@@ -17,3 +17,23 @@ var findMaxAverage = function (nums, k) {
 
   return maxAvg;
 };
+
+// Sliding Window approach
+// TC => O(n), SC => O(1)
+
+var findMaxAverage = function (nums, k) {
+  let windowSum = 0;
+
+  for (let i = 0; i < k; i++) {
+    windowSum += nums[i];
+  }
+
+  let maxSum = windowSum;
+
+  for (let i = k; i < nums.length; i++) {
+    windowSum += nums[i] - nums[i - k];
+    maxSum = Math.max(maxSum, windowSum);
+  }
+
+  return maxSum / k;
+};
