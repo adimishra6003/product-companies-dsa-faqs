2f4dd27431744e60894cea2ea351589fe0a130cb
adimishra6003 <adityamishra120799@gmail.com>
2025-11-10 23:56:50 +0530

    recursive approach

--- Standard Algos/sum-of-digits.js
+++ Standard Algos/sum-of-digits.js
@@ -12,3 +12,16 @@ function sumOfDigitsIterative(n) {
 }
 
 console.log(sumOfDigitsIterative(1236));
+
+// Recursive approach
+// TC => O(n) SC => O(n) because of Call stack
+
+function sumOfDigitsRecursive(n) {
+  if (n == 0) return 0;
+
+  return (n % 10) + sumOfDigitsRecursive(Math.floor(n / 10));
+}
+
+console.log(sumOfDigitsRecursive(1236));
+
+// No DP solution to this because no overlapping subproblems here
