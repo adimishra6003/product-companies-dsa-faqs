70f4cc16fed7633682e7db7202315da6a8160c95
adimishra6003 <adityamishra120799@gmail.com>
2025-11-17 16:03:57 +0530

    iterative apporach

--- Standard Algos/Trees/height-or-depth-of-binary-tree.js
+++ Standard Algos/Trees/height-or-depth-of-binary-tree.js
@@ -1,7 +1,32 @@
-function height(node) {
+// Recursive approach
+// TC => O(n) SC => O(n)
+
+function heightRecursive(node) {
   if (!node) return 0;
 
-  return 1 + Math.max(height(node.left), height(node.right));
+  return 1 + Math.max(heightRecursive(node.left), heightRecursive(node.right));
+}
+// Iterative approach
+// TC => O(n) SC => O(n)
+
+function heightIterative(node) {
+  if (!node) return 0;
+
+  let q = [node];
+  let height = 0;
+
+  while (q.length) {
+    let numberOfNodesInThisLevel = q.length;
+
+    while (numberOfNodesInThisLevel--) {
+      let curr = q.shift();
+      if (curr.left) q.push(curr.left);
+      if (curr.right) q.push(curr.right);
+    }
+    height++;
+  }
+
+  return height;
 }
 
 // Testing these using a custom tree
@@ -16,4 +41,5 @@ class TreeNode {
 
 let root = new TreeNode(1, null, new TreeNode(2, new TreeNode(3)));
 
-console.log(height(root));
+console.log(heightRecursive(root));
+console.log(heightIterative(root));
